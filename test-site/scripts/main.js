5a91709e1b1148e6878e5d58c8fb95528c9261b4
Peter Williamson <p.williamson734@gmail.com>
2021-04-15 18:17:26 +0200

    nothing

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -2,9 +2,25 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/Viking.jpeg') {
+    if(mySrc === 'images/Viking.jpg') {
       myImage.setAttribute('src','images/Viking2.jpeg');
     } else {
-      myImage.setAttribute('src','images/Viking.jpeg');
+      myImage.setAttribute('src','images/Viking.jpg');
     }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Vikings are awesome, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Vikings are awesome,' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
 }
